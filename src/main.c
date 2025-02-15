6198edf2a4fddc74e354c4dda99c63daa84a1ad4
noelnimstad <noel@nimstad.com>
2025-02-15 18:14:02 +0100

    test after rename

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 int main(int argc, char const *argv[])
 {
 	ASSERT(argc >= 3, "Not enough arguments supplied.\n\tProper usage: qlang <IN.Q> <OUT>\n");
-	processFile(argv[1]); // Process main program file
+	processFile(argv[1]); // Process main supplied file
 
 	return EXIT_SUCCESS;
 };
